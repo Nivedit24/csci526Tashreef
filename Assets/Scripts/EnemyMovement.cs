4d7c1c282093606edaf6224b721c0ef72946af10
Pradnesh <pradneshnaik14@gmail.com>
2023-10-15 20:10:59 -0700

    implemented the recommended changes

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TornadoMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
     public float speed = 10f;
     void Start()
