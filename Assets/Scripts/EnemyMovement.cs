9b26ef8422cc19bc564ea498ef014720a90b3692
Sohail-Yawer <sohailyawer9@gmail.com>
2023-11-02 00:10:36 -0700

    freeze and unfreeze demon

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,6 +7,8 @@ public class EnemyMovement : MonoBehaviour
     public float speed = 10f;
     public GameObject projectilePrefab;
     public Transform[] LaunchPoints;
+
+    private bool isFrozen = false;
     // Update is called once per frame
     void Update()
     {
@@ -43,6 +45,24 @@ public class EnemyMovement : MonoBehaviour
         {
             Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
         }
+
+        if( transform.gameObject.tag == "Demon" && collision.gameObject.tag == "PlayerSnowBall")
+        {
+            Debug.Log("Demon got hit by snowball");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (transform.gameObject.tag == "Demon" && collision.gameObject.tag == "PlayerSnowBall")
+        {
+            Debug.Log("Demon got hit by snowball");
+            isFrozen = true;
+            StartCoroutine(FreezeAndUnfreeze());
+            OnDisable();
+            Destroy(collision.gameObject);
+        }
+
     }
 
     void LaunchProjectiles()
@@ -51,5 +71,17 @@ public class EnemyMovement : MonoBehaviour
         Quaternion rotation = gameObject.tag == "EarthMonster" ? Quaternion.identity : LaunchPoints[dirIndex].rotation;
         GameObject instantiatedPrefab = Instantiate(projectilePrefab, LaunchPoints[dirIndex].position, rotation);
         instantiatedPrefab.GetComponent<EnemyProjectile>().enemy = this.gameObject;
+    }
+
+    IEnumerator FreezeAndUnfreeze()
+    {
+        // Freeze the demon for 5 seconds
+        speed = 0f;
+        yield return new WaitForSeconds(5f);
+
+        // Unfreeze the demon and start moving again
+        isFrozen = false;
+        speed = 10f; // Set speed to its absolute value
+        OnEnable();
     }
 }
--- Assets/Scripts/snowBallSelfDestruct.cs
+++ Assets/Scripts/snowBallSelfDestruct.cs
@@ -19,7 +19,7 @@ public class snowBallSelfDestruct : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         string collisionTag = collision.gameObject.tag;
-        if(collisionTag != "AcidDrop" && collisionTag != "IceMonster")
+        if(collisionTag != "AcidDrop" && collisionTag != "IceMonster" && collisionTag!="Demon")
         {
             Destroy(transform.gameObject);
         }
