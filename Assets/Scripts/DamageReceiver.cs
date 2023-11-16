a8598e6b5417e3557b0c9cc3e90c5ff8939f7f1c
Pradnesh <pradneshnaik14@gmail.com>
2023-11-16 15:34:30 -0800

    heart on enemy death

--- Assets/Scripts/DamageReceiver.cs
+++ Assets/Scripts/DamageReceiver.cs
@@ -58,4 +58,10 @@ public class DamageReceiver : MonoBehaviour
         }
         StartCoroutine(InvincibilityFrame());
     }
+
+    public void giveHealth()
+    {
+        healthBar.SetMaxHealth(maxHealth);
+        currHealth = maxHealth;
+    }
 }
--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -8,10 +8,12 @@ public class EnemyDamage : MonoBehaviour
     public HealthModifier HealthBar;
     public int maxHealth = 100;
     public int currHealth;
+    public bool giveHeart;
     void Start()
     {
         HealthBar.SetMaxHealth(maxHealth);
         currHealth = maxHealth;
+        giveHeart = Random.Range(0, 2) == 0 ? true : false; 
     }
 
     // Update is called once per frame
